4a37d7d836500f22150653c41885cd46d88cbaea
Juegoman <gallegos_ruben@yahoo.com>
2021-10-04 08:42:56 -0700

    fixed title

--- src/GameScene.js
+++ src/GameScene.js
@@ -9,6 +9,7 @@ import zap from './assets/zap.wav';
 import boom from './assets/boom.wav';
 import strike from './assets/strike.wav';
 import jet from './assets/jet.wav';
+import song from './assets/song.mp3';
 
 import World from './World';
 import Player from './Player';
@@ -36,6 +37,7 @@ export default class GameScene extends Phaser.Scene {
         this.load.audio('boom', boom);
         this.load.audio('strike', strike);
         this.load.audio('jet', jet);
+        this.load.audio('song', song);
         this.load.spritesheet('character', characterImage, { frameWidth: 150, frameHeight: 150 });
         this.load.spritesheet('zapper', zapperImage, { frameWidth: 125, frameHeight: 100 });
         this.load.spritesheet('spark', sparkImage, { frameWidth: 30, frameHeight: 30 });
@@ -47,10 +49,14 @@ export default class GameScene extends Phaser.Scene {
         this.gameModules.enemy = new Enemy(this.gameModules, this);
         this.gameModules.world = new World(this.gameModules, this);
         this.gameModules.hitspark = new Hitspark(this.gameModules, this);
+
+        this.sound.stopAll();
+        this.sound.play('song', { volume: 0.5, loop: true });
+        
         this.sys.canvas.addEventListener('click', () => {
           if (this.sys.isPaused())  {
             this.sys.resume();
-            // this.sound.play('song', { volume: 0.5, loop: true });
+            this.sound.play('song', { volume: 0.5, loop: true });
           }
         });
         document.addEventListener('keypress', () => {
--- src/TitleScene.js
+++ src/TitleScene.js
@@ -1,28 +1,26 @@
 import titleImage from './assets/titlescreen.png';
-// import song from "./assets/LDgametrack.mp3";
+import song from "./assets/song.mp3";
 
 export default class TitleScene extends Phaser.Scene {
     constructor() {
         super({ key: 'title' });
     }
     preload() {
-        this.load.image('title', titleImage);
-        // this.load.audio('song', song);
+        this.load.spritesheet('title', titleImage, { frameWidth: 800, frameHeight: 600 });
+        this.load.audio('song', song);
     }
     create() {
-        // this.sound.play('song', { volume: 0.5, loop: true });
-        const splash = this.add.sprite(0, 0, 'title');
+        this.sound.play('song', { volume: 0.5, loop: true });
+        const splash = this.add.sprite(0, 0, 'title', 0);
         splash.setOrigin(0,0);
-        splash.setInteractive({ useHandCursor: true });
         this.anims.create({
-            key: 'titlescreen1',
-            frames: this.anims.generateFrameNumbers('title', { frames: [0,1, 2, 3, 4, 5, 6, 7, 8] }),
+            key: 'titlescreen',
+            frames: this.anims.generateFrameNumbers('title', { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
             frameRate: 3,
             repeat: 1
         })
-        splash.play('titlescreen1')
+        splash.play('titlescreen')
         this.input.keyboard.on('keydown', () => {
-            console.log('hello')
             this.scene.switch('game');
         })
     }
